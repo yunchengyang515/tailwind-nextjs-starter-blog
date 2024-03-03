710e24f6b6129511cc14cdb9828652d5dec101b8
Yuncheng <fabioyang781@gmail.com>
2024-03-04 01:19:42 +1100

    Regenerated tags

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,26 +1,16 @@
 interface Project {
-  title: string,
-  description: string,
-  href?: string,
-  imgSrc?: string,
+  title: string
+  description: string
+  href?: string
+  imgSrc?: string
 }
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
+    title: 'A BIM Integration',
+    description: `Easily use this BIM model to create material passports and track the EOL construction materials.`,
     imgSrc: '/static/images/google.png',
     href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
   },
 ]
 
